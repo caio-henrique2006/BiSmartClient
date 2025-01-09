5d489bcd22f5ef34590da89ec6093d2ff335d0d1
caio-henrique2006 <caio_henrique2006@outlook.com>
2025-01-09 14:48:03 -0300

    sistema de busca de dados no bd local refatorado

--- db.js
+++ db.js
@@ -8,58 +8,30 @@ class DB {
         database: "geral"
     }
     #SQL_commands = {
-        valor_vendas: "SELECT sum(totgeral) FROM movvendas WHERE cancelada = 'N' AND emissao BETWEEN '" + data_inicio + "' AND '" + data_fim + "';",
-        valor_compras: "SELECT sum(totgeral) FROM MOVENT WHERE EMISSAO BETWEEN '" + data_inicio + "' AND '" + data_fim + "' AND CANCELADA !='S';",
-        quantidade_vendas: "SELECT count(docum) FROM movvendas WHERE cancelada = 'N' AND emissao BETWEEN '" + data_inicio + "' AND '" + data_fim + "';",
-        ticket_medio: "SELECT AVG(totgeral) FROM movvendas WHERE cancelada = 'N' AND emissao BETWEEN '" + data_inicio + "' AND '" + data_fim + "';"
+        valor_vendas: "SELECT sum(totgeral) FROM movvendas WHERE cancelada = 'N' AND emissao BETWEEN ? AND ?;",
+        valor_compras: "SELECT sum(totgeral) FROM MOVENT WHERE EMISSAO BETWEEN ? AND ? AND CANCELADA !='S';",
+        quantidade_vendas: "SELECT count(docum) FROM movvendas WHERE cancelada = 'N' AND emissao BETWEEN ? AND ?;",
+        ticket_medio: "SELECT AVG(totgeral) FROM movvendas WHERE cancelada = 'N' AND emissao BETWEEN ? AND ?;"
     }
 
     async getData(data_inicio, data_fim) {
-        const valor_vendas = await this.getValorVendas(data_inicio, data_fim);
-        const valor_compras = await this.getValorCompras(data_inicio, data_fim);
-        const quantidade_vendas = await this.getQuantidadeVendas(data_inicio, data_fim);
-        const ticket_medio = await this.getTicketMedio(data_inicio, data_fim);
+        const parameters = [data_inicio, data_fim];
+        const valor_vendas = await this.executeSQLCommand("valor_vendas", this.#SQL_commands.valor_vendas, parameters);
+        const valor_compras = await this.executeSQLCommand("valor_compras", this.#SQL_commands.valor_compras, parameters);
+        const quantidade_vendas = await this.executeSQLCommand("quantidade_vendas", this.#SQL_commands.quantidade_vendas, parameters);
+        const ticket_medio = await this.executeSQLCommand("ticket_medio", this.#SQL_commands.ticket_medio, parameters);
         const data = Object.assign({}, valor_vendas, valor_compras, quantidade_vendas, ticket_medio);
         return data;
     }
 
-    async executeSQLCommand(label, command) {
+    async executeSQLCommand(label, command, parameters) {
         const conn = await this.#connect();
-        let [rows, fields] = await conn.query(command);
+        let [rows, fields] = await conn.query(command, parameters);
+        const value = rows[0][Object.keys(rows[0])[0]] ? rows[0][Object.keys(rows[0])[0]] : 0;
+        const response = {[label]: value};
         conn.end();
+        return response;
     } 
-
-    async getValorVendas(data_inicio, data_fim) {
-        const conn = await this.#connect();
-        let [rows, fields] = await conn.query("SELECT sum(totgeral) FROM movvendas WHERE cancelada = 'N' AND emissao BETWEEN '" + data_inicio + "' AND '" + data_fim + "';");
-        const value = rows[0]["sum(totgeral)"] ? rows[0]["sum(totgeral)"] : 0;
-        const response = {"valor_vendas": value};
-        return response;
-    }
-
-    async getValorCompras(data_inicio, data_fim) {
-        const conn = await this.#connect();
-        let [rows, fields] = await conn.query("SELECT sum(totgeral) FROM MOVENT WHERE EMISSAO BETWEEN '" + data_inicio + "' AND '" + data_fim + "' AND CANCELADA !='S';");
-        const value = rows[0]["sum(totgeral)"] ? rows[0]["sum(totgeral)"] : 0;
-        const response = {"valor_compras": value};
-        return response;
-    }
-
-    async getQuantidadeVendas(data_inicio, data_fim) {
-        const conn = await this.#connect();
-        let [rows, fields] = await conn.query("SELECT count(docum) FROM movvendas WHERE cancelada = 'N' AND emissao BETWEEN '" + data_inicio + "' AND '" + data_fim + "';");
-        const value = rows[0]["count(docum)"] ? rows[0]["count(docum)"] : 0;
-        const response = {"quantidade_vendas": value};
-        return response;
-    }
-
-    async getTicketMedio(data_inicio, data_fim) {
-        const conn = await this.#connect();
-        let [rows, fields] = await conn.query("SELECT AVG(totgeral) FROM movvendas WHERE cancelada = 'N' AND emissao BETWEEN '" + data_inicio + "' AND '" + data_fim + "';");
-        const value = rows[0]["AVG(totgeral)"] ? rows[0]["AVG(totgeral)"] : 0;
-        const response = {"ticket_medio": value};
-        return response;
-    }
 
     async #connect() {
         return await mysql.createConnection(this.#connection_data);
