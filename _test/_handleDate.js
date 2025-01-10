0a2c8b7961c6b035199b7d338655ea49c3e635cd
caio-henrique2006 <caio_henrique2006@outlook.com>
2025-01-10 17:48:23 -0300

    add tests to handleDate

--- _test/_handleDate.js
+++ _test/_handleDate.js
@@ -1,19 +1,95 @@
 const HandleDate = require("../scripts/handleDate.js");
+const _test = require("./_test.js");
 
 class _handleDate {
     runTests() {
         Object.keys(this.#tests).forEach((test) => {
+            _test.log_start_test("Teste: " + test);
             this.#tests[test]();
+            _test.log_end_test();
         })
     }
 
     #tests = {
-        test_func: () => {
+        checkDate: () => {
             const handleDate = new HandleDate();
-            console.log("Teste feito");
+            const test_cases = [
+                {
+                    data_inicio: "",
+                    data_fim: "",
+                    expected: false
+                },
+                {
+                    data_inicio: "",
+                    data_fim: "2024-01",
+                    expected: false
+                },
+                {
+                    data_inicio: null,
+                    data_fim: null,
+                    expected: false
+                },
+                {
+                    data_inicio: 2024,
+                    data_fim: 2024,
+                    expected: false
+                },
+                {
+                    data_inicio: "2024-01",
+                    data_fim: "2024-00",
+                    expected: false
+                },
+                {
+                    data_inicio: "2024-01",
+                    data_fim: "2024-01",
+                    expected: true
+                },
+                {
+                    data_inicio: "2024-01-01",
+                    data_fim: "2024-01-01",
+                    expected: false
+                },
+                {
+                    data_inicio: "2024-03-",
+                    data_fim: "2024-04-",
+                    expected: false
+                },
+                {
+                    data_inicio: "2024-13",
+                    data_fim: "2024-12",
+                    expected: false
+                },
+                {
+                    data_inicio: "2024-10",
+                    data_fim: "2024-09",
+                    expected: false
+                },
+                {
+                    data_inicio: "2023-09",
+                    data_fim: "2024-09",
+                    expected: true
+                },
+            ]
+            let counter = 0;
+            test_cases.forEach((test) => {
+                try {
+                    counter += 1;
+                    if (test.expected == handleDate.checkDate(test.data_inicio, test.data_fim)) {
+                        _test.log_passed(counter);
+                    } else {
+                        _test.log_exception(counter);
+                    }
+                } catch (e) {
+                    if (test.expected) {
+                        _test.log_exception(counter);
+                    } else {
+                        _test.log_passed(counter);
+                    }
+                } 
+            })
         }
     }
 }
 
-const test = new _handleDate();
-test.runTests();
+const instance = new _handleDate();
+instance.runTests();
--- scripts/handleDate.js
+++ scripts/handleDate.js
@@ -1,22 +1,28 @@
+const { isMatch, isBefore, isEqual } = require("date-fns");
 
 class HandleDate {
+    month_format = "yyyy-MM";
+
     checkDate(data_inicio, data_fim) {
-        if (
-        this.IsFormatMonth(data_inicio, data_fim) &&
-        this.inicioIsBeforeFim(data_inicio, data_fim)
-        ) {
-            return true;
-        } else {
+        try {
+            return (
+                this.isFormatMonth(data_inicio, data_fim) &&
+                this.inicioIsBeforeFim(data_inicio, data_fim)
+            );
+        } catch (e) {
             return false;
         }
     }
 
     isFormatMonth(data_inicio, data_fim) {
-
+        return (
+            isMatch(data_inicio, this.month_format) &&
+            isMatch(data_fim, this.month_format)
+        );
     }
 
     inicioIsBeforeFim(data_inicio, data_fim) {
-
+        return (isBefore(data_inicio, data_fim) || isEqual(data_inicio, data_fim));
     }
 }
 
