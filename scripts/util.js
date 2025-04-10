56ca5b7024671d75afc297f398aff7c7ff2e7235
caio-henrique2006 <caio_henrique2006@outlook.com>
2025-04-10 14:48:24 -0300

    Add: - Detecção de mudanças no banco de dados em relação ao último envio

--- scripts/util.js
+++ scripts/util.js
@@ -1,10 +1,25 @@
 const util = {
   isDataDifferent(data_1, data_2) {
     try {
-      Object.keys(data_1).forEach((item) => {
-        console.log("COMPARE: ", data_1[item], data_2[item]);
-        if (data_1[item] !== data_2[item]) {
-          throw "err";
+      Object.keys(data_1).forEach((key) => {
+        if (
+          typeof data_1[key] === "object" &&
+          typeof data_2[key] === "object"
+        ) {
+          const response = this.isDataDifferent(data_1[key], data_2[key]);
+          if (response) {
+            throw "Erro: Diferentes";
+          }
+        } else if (
+          (typeof data_1[key] === "number" &&
+            typeof data_2[key] === "number") ||
+          (typeof data_1[key] === "string" && typeof data_2[key] === "string")
+        ) {
+          if (data_1[key] !== data_2[key]) {
+            throw "Erro: Diferentes";
+          }
+        } else {
+          throw "Erro: Diferentes";
         }
       });
       return false;
@@ -13,5 +28,23 @@ const util = {
     }
   },
 };
+
+/*
+Object.keys(data).forEach((key) => {
+    if (summed_obj[key]) {
+      if (
+        typeof summed_obj[key] == "number" &&
+        typeof data[key] == "number"
+      ) {
+        summed_obj[key] += data[key];
+      } else if (typeof summed_obj[key] == "object") {
+        summed_obj[key] = this.#recur_sum_data(summed_obj[key], data[key]);
+      }
+    } else {
+      summed_obj[key] = data[key];
+    }
+  });
+  return summed_obj;
+*/
 
 module.exports = util;
