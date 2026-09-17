21bd8f686e4dce9c08c4a2c31452035d3ee319bd
caio-henrique2006 <caio_henrique2006@outlook.com>
2026-09-17 01:42:06 -0300

    Adiciona --em andamento (Estilo página principal): Estruturação de estilo e fonte

--- scripts/server.js
+++ scripts/server.js
@@ -1,12 +1,12 @@
-const { app } = require("electron");
-const fs = require("fs");
-const path = require("node:path");
+const { app } = require('electron');
+const fs = require('fs');
+const path = require('node:path');
 
 class Server {
-  #email = "";
-  #password = "";
-  #server_url = "";
-  #server_route = "client_send?client_origin=desktop";
+  #email = '';
+  #password = '';
+  #server_url = '';
+  #server_route = 'client_send?client_origin=desktop';
 
   constructor() {
     this.init();
@@ -14,10 +14,10 @@ class Server {
 
   async init() {
     try {
-      const path_userData = app.getPath("userData");
-      const filePath = path.join(path_userData, "server_login.json");
-      const data = JSON.parse(fs.readFileSync(filePath, { encoding: "utf8" }));
-      console.log("server_login: ", data);
+      const path_userData = app.getPath('userData');
+      const filePath = path.join(path_userData, 'server_login.json');
+      const data = JSON.parse(fs.readFileSync(filePath, { encoding: 'utf8' }));
+      console.log('server_login: ', data);
       this.#email = data.email;
       this.#password = data.password;
       this.#server_url = data.server_url;
@@ -28,16 +28,16 @@ class Server {
 
   async sendDataToServer(data) {
     try {
-      console.log("URL: ", this.#server_url + this.#server_route);
-      console.log("Email: ", this.#email);
-      console.log("Password: ", this.#password);
-      console.log("DATA: ", data);
+      console.log('URL: ', this.#server_url + this.#server_route);
+      console.log('Email: ', this.#email);
+      console.log('Password: ', this.#password);
+      console.log('DATA: ', data);
       const response = await fetch(this.#server_url + this.#server_route, {
-        method: "POST",
-        mode: "cors",
+        method: 'POST',
+        mode: 'cors',
         body: JSON.stringify(data),
         headers: {
-          "Content-type": "application/json",
+          'Content-type': 'application/json',
           email: this.#email,
           password: this.#password,
         },
@@ -48,11 +48,11 @@ class Server {
       console.log(response.status);
       switch (response.status) {
         case 200:
-          return "Enviado com sucesso";
+          return 'Enviado com sucesso';
         case 401:
-          throw "Erro: Nao autorizado. Cheque os dados de login";
+          throw 'Erro: Nao autorizado. Cheque os dados de login';
         default:
-          throw "Erro: Problema interno";
+          throw 'Erro: Problema interno';
       }
     } catch (e) {
       return e;
